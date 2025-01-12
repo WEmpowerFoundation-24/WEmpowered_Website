51b3c145e8430df1e4272660ee22a0e36e9b7edd
Adarsh <ashu2100ag@gmail.com>
2025-01-12 18:58:11 +0530

    changed the name in navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ export default function Navbar() {
     <>
       <div className=" absolute flex flex-row w-screen  p-8 justify-between text-gray-400 items-center z-10 lg:px-20">
         <a className="text-white cursor-pointer font-Gilroy-700 text-xl" href="/">
-          WEmpowered
+          WEmpower
         </a>
         <Navlist />
       </div>
