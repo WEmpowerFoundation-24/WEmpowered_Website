cfc3f4676b3b41be33b587c1bc616510fef02706
Adarsh <ashu2100ag@gmail.com>
2025-05-11 20:23:09 +0530

    added contact us data into mongoDB

--- server/db/connectDB.js
+++ server/db/connectDB.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const connectDB = async()=>{
     try{
-        const conn = await mongoose.connect('mongodb://127.0.0.1:27017/WEmpower')
+        const conn = await mongoose.connect('mongodb+srv://Wempower9023sfjbjf:sjhdbhjcsbdchsjdcvsjvcvacjhc@cluster0.h2e9uwo.mongodb.net/email')
         console.log(`MongoDB connected: ${conn.connection.host}`);
     }catch(error){
         console.log("MongoDB connection error: ",error.message)
