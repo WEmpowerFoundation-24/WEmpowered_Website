cc9f5414e16733f8e5736c6a0ea6bd2f4d8b24d6
Adarsh <ashu2100ag@gmail.com>
2025-05-11 20:51:15 +0530

    solving error

--- server/db/connectDB.js
+++ server/db/connectDB.js
@@ -1,11 +1,14 @@
 const mongoose = require("mongoose");
-const connectDB = async()=>{
-    try{
-        const conn = await mongoose.connect('mongodb+srv://Wempower9023sfjbjf:sjhdbhjcsbdchsjdcvsjvcvacjhc@cluster0.h2e9uwo.mongodb.net/email')
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect('mongodb+srv://Wempower9023sfjbjf:sjhdbhjcsbdchsjdcvsjvcvacjhc@cluster0.h2e9uwo.mongodb.net/email?retryWrites=true&w=majority', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
         console.log(`MongoDB connected: ${conn.connection.host}`);
-    }catch(error){
-        console.log("MongoDB connection error: ",error.message)
+    } catch (error) {
+        console.log("MongoDB connection error: ", error.message)
         process.exit(1);
     }
 }
-module.exports = connectDB;
+module.exports = connectDB; 
